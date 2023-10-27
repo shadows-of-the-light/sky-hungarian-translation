5de2dd1fc38a1cfa9c778d26fb8a38d11823e633
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2023-10-27 14:12:21 +0200

    Progress tracker tool

---
+++ getProgress.js
@@ -0,0 +1,29 @@
+const gameVersion = '0.23.4 235043';
+
+const fs = require('fs');
+const path = require('path');
+
+const file1 = path.join(__dirname, 'translations', gameVersion, 'Localizable.strings');
+const file2 = path.join(__dirname, 'translations', gameVersion, 'original.strings');
+
+const file1Lines = fs.readFileSync(file1, 'utf8').split('\n');
+const file2Lines = fs.readFileSync(file2, 'utf8').split('\n');
+
+const auroraLiricsRegex = /^"aurora_\w*_[0-9]{2,}".*/;
+
+let requiredLines = 0;
+let count = 0;
+for (let i = 0; i < file1Lines.length; i++) {
+  const isNotComment = !file1Lines[i].startsWith('/');
+  const isNotAuraLirics = !auroraLiricsRegex.test(file1Lines[i]);
+  if (isNotComment && isNotAuraLirics) {
+    requiredLines++;
+    if (file1Lines[i] !== file2Lines[i]) {
+      count++;
+    }
+  }
+}
+
+const percent = Math.round((count / requiredLines) * 100);
+
+console.log(`Progress for game version ${gameVersion}:\n${count} / ${requiredLines} (${percent}%)`);
